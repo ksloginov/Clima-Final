6adae05354c5b86e51997cbd6f3d6c6df515ef54
Konstantin Loginov <loginov.k@gmail.com>
2021-09-22 20:20:54 +0200

    Update Endpoint.swift

--- Clima-StartingKit/DataService/Endpoint.swift
+++ Clima-StartingKit/DataService/Endpoint.swift
@@ -18,9 +18,9 @@ enum Endpoint {
         switch self {
         case .forecast(let city):
             let encodedCity = city.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? city
-            return "\(Endpoint.host)?q=\(encodedCity)&appid=9067ea08c6a8b9bce1efaf060de4246c&units=metric"
+            return "\(Endpoint.host)?q=\(encodedCity)&appid=<insertkey>&units=metric"
         case .localForecast(let latitude, let longitude):
-            return "\(Endpoint.host)?lat=\(latitude)&lon=\(longitude)&appid=9067ea08c6a8b9bce1efaf060de4246c&units=metric"
+            return "\(Endpoint.host)?lat=\(latitude)&lon=\(longitude)&appid=<insertkey>&units=metric"
         }
     }
 }
