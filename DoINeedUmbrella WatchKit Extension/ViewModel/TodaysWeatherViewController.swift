6638e75621c50791e249dab601241f2a130e6680
Konstantin Loginov <loginov.k@gmail.com>
2021-10-10 13:06:21 +0200

    Step 5: In the ViewModel, doing a network request to determine if the umbrella is needed

--- DoINeedUmbrella WatchKit Extension/ViewModel/TodaysWeatherViewController.swift
+++ DoINeedUmbrella WatchKit Extension/ViewModel/TodaysWeatherViewController.swift
@@ -12,7 +12,7 @@ import SwiftUI
 enum State {
     case loading
     case error(Error)
-    case value(needUmbrella: Bool, currentWeather: Weather)
+    case value(needUmbrella: Bool, currentWeather: WeatherState)
 }
 
 class TodaysWeatherViewController: NSObject, ObservableObject, CLLocationManagerDelegate {
@@ -31,14 +31,34 @@ class TodaysWeatherViewController: NSObject, ObservableObject, CLLocationManager
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             if let url = URL(string: Endpoint.todaysForecast(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude).url) {
-                URLSession.shared.dataTask(with: url) { data, _, error in
+                URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
                     
+                    if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
+                        DispatchQueue.main.async { self?.state = .error(NetworkError.failedRequest) }
+                        return
+                    } else if let error = error {
+                        DispatchQueue.main.async { self?.state = .error(error) }
+                        return
+                    }
+                    
+                    if let data = data {
+                        do {
+                            let decodedData = try JSONDecoder().decode(TodayWeatherData.self, from: data)
+                            DispatchQueue.main.async {
+                                self?.state = .value(needUmbrella: decodedData.hourly.prefix(10).contains(where: {$0.weatherCondition?.isRaining == true }), currentWeather: decodedData.current)
+                            }
+                            
+                        } catch {
+                            DispatchQueue.main.async { self?.state = .error(error) }
+                            return
+                        }
+                    }
                 }
             }
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error)
+        state = .error(error)
     }
 }
